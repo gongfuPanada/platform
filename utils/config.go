d383dbd3873929f700cdfa41b7d67309ba95aa6d
Elias Nahum <nahumhbl@gmail.com>
2016-06-24 16:13:49 -0400

    PLT-3137 Support for SAML configuration

--- utils/config.go
+++ utils/config.go
@@ -276,6 +276,11 @@ func getClientConfig(c *model.Config) map[string]string {
 		if *License.Features.Compliance {
 			props["EnableCompliance"] = strconv.FormatBool(*c.ComplianceSettings.Enable)
 		}
+
+		if *License.Features.SAML {
+			props["EnableSaml"] = strconv.FormatBool(*c.SamlSettings.Enable)
+			props["SamlLoginButtonText"] = *c.SamlSettings.LoginButtonText
+		}
 	}
 
 	return props
